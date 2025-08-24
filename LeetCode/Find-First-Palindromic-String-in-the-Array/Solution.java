9aaf5a8b51e2e961d61dfbadee7d16f7390f92d1
metihimabindu <153541711+metihimabindu@users.noreply.github.com>
2025-08-24 23:02:37 +0530

    Add solution for Find First Palindromic String in the Array

--- LeetCode/Find-First-Palindromic-String-in-the-Array/Solution.java
+++ LeetCode/Find-First-Palindromic-String-in-the-Array/Solution.java
@@ -3,7 +3,6 @@ class Solution {
         for(int i=0;i<words.length;i++){
              StringBuilder str=new StringBuilder(words[i]).reverse();
              String str1=str.toString();
-             System.out.println(str1 +" "+words[i]);
             if(words[i].equals(str1)){
                 return words[i];
             }
