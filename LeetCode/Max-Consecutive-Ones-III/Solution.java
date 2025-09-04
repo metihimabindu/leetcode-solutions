c3c6bded3bd9a3fd654330c59e3f5b204285553a
metihimabindu <153541711+metihimabindu@users.noreply.github.com>
2025-09-04 18:54:01 +0530

    Add solution for Max Consecutive Ones III

--- LeetCode/Max-Consecutive-Ones-III/Solution.java
+++ LeetCode/Max-Consecutive-Ones-III/Solution.java
@@ -1,7 +1,23 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int sum=0;
-        if(k>=nums.length){
+        int sum=0;int left=0,right=0,count=0;
+        while(right<nums.length){
+             if(nums[right]==0){
+               count++;
+            }
+             if(count<=k){
+               sum=Math.max(sum,right-left+1);
+            }else{
+                right=left;
+                left++;
+                count=0;
+            }
+            right++;
+        }
+        
+        return sum;
+    }
+    /*if(k>=nums.length){
             return nums.length;
         }
         for(int left=0;left<nums.length;left++){
@@ -16,7 +32,5 @@ class Solution {
                 break;
             }
             }
-        }
-        return sum;
-    }
+        }*/
 }
