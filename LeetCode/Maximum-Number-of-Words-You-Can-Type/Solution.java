e2f658323872ea87c90083f3e6644b5796d03bc7
metihimabindu <153541711+metihimabindu@users.noreply.github.com>
2025-09-15 16:44:53 +0530

    Add solution for Maximum Number of Words You Can Type

---
+++ LeetCode/Maximum-Number-of-Words-You-Can-Type/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        String []str=text.split(" ");
+        HashSet<String> l=new HashSet<>();
+        int c=0;
+        for(String val:str){
+            int count=0;
+            for(int i=0;i<val.length();i++){
+                for(int j=0;j<brokenLetters.length();j++){
+                    if(val.charAt(i)==brokenLetters.charAt(j)){
+                        count++;
+                    }
+                }
+            }
+            if(count==0) c++;
+        }
+        return c;
+    }
+}
