13f472e743e89aeaa98247b4af46404a5f023022
metihimabindu <153541711+metihimabindu@users.noreply.github.com>
2025-10-13 22:24:35 +0530

    Add solution for Break a Palindrome

---
+++ LeetCode/Break-a-Palindrome/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+      char []arr=palindrome.toCharArray();
+      if(arr.length==1){return "";}
+      for(int i=0;i<arr.length/2;i++){
+        if(arr[i]!='a'){arr[i]='a';return new String(arr);}
+      }
+      arr[arr.length-1]='b';
+      return new String(arr);
+    }
+}
