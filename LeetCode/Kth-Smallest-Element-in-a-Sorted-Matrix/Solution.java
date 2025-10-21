1114cfac7b0bd390c9a3bf49d4106e70379e56cf
metihimabindu <153541711+metihimabindu@users.noreply.github.com>
2025-10-21 21:30:13 +0530

    Add solution for Kth Smallest Element in a Sorted Matrix

---
+++ LeetCode/Kth-Smallest-Element-in-a-Sorted-Matrix/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        List<Integer> l1=new ArrayList<>();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                l1.add(matrix[i][j]);
+            }
+        }
+        Collections.sort(l1);
+        System.out.println(l1);
+        return l1.get(k-1);
+    }
+}
