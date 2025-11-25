b274bd4c8742efb36232d541142f2ccbdcecd6fa
metihimabindu <153541711+metihimabindu@users.noreply.github.com>
2025-11-25 17:21:23 +0530

    Add solution for Unique Paths

---
+++ LeetCode/Unique-Paths/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int [][]dp=new int[m][n];
+        for(int i=0;i<m;i++)Arrays.fill(dp[i],-1);
+        return back(m,n,m-1,n-1,dp);
+    }
+    public static int back(int m,int n,int i,int j,int [][]dp){
+        if(i==0 && j==0){return 1;}
+        if(i<0 || j<0){return 0;}
+        if(dp[i][j]!=-1){return dp[i][j];}
+        int left=back(m,n,i-1,j,dp);
+        int right=back(m,n,i,j-1,dp);
+        return dp[i][j]=left+right;
+    }
+}
