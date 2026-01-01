1c04406fab60210c05c0313bd5ca14bcd81f8227
metihimabindu <153541711+metihimabindu@users.noreply.github.com>
2026-01-01 17:53:03 +0530

    Add solution for Plus One

--- LeetCode/Plus-One/Solution.java
+++ LeetCode/Plus-One/Solution.java
@@ -1,18 +1,19 @@
-import java.util.ArrayList;
-
-class Solution {
-    public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                System.out.println(digits[i]);
-                return digits;
-            }
-            digits[i] = 0;
-        }
-        int[] result = new int[n + 1];
-        result[0] = 1;
-        return result;
-    }
-}
+1import java.util.ArrayList;
+2
+3class Solution {
+4    public int[] plusOne(int[] digits) {
+5        int n = digits.length;
+6        for (int i = n - 1; i >= 0; i--) {
+7            if (digits[i] < 9) {
+8                digits[i]++;
+9                System.out.println(digits[i]);
+10                return digits;
+11            }
+12            digits[i] = 0;
+13        }
+14        int[] result = new int[n + 1];
+15        result[0] = 1;
+16        return result;
+17    }
+18}
+19
