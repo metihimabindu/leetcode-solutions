dd2afe9201d7794c6481d6a46f359639a12b7206
metihimabindu <153541711+metihimabindu@users.noreply.github.com>
2026-01-06 22:44:47 +0530

    Add solution for List the Products Ordered in a Period

---
+++ LeetCode/List-the-Products-Ordered-in-a-Period/Solution.sql
@@ -0,0 +1,7 @@
+SELECT p.product_name, SUM(o.unit) as unit
+FROM Products p
+JOIN Orders o
+ON p.product_id = o.product_id
+WHERE o.order_date LIKE '2020-02-%'
+GROUP BY p.product_id
+HAVING unit >= 100;
